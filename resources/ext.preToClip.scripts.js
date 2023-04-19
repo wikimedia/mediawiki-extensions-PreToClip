e557ae4ab4839388dd9760f1a148965a79124c70
rvogel <vogel@hallowelt.biz>
2023-04-19 09:59:17 +0200

    Fix positioning and position of the button

--- resources/ext.preToClip.scripts.js
+++ resources/ext.preToClip.scripts.js
@@ -1,61 +1,70 @@
-function createPreToClipButton( pre ) {
-	var copyButton = document.createElement( 'button' ),
-		preRect = pre.getBoundingClientRect(),
-		preUpperRightCornerX = preRect.x + preRect.width;
-
-	copyButton.assignedPre = pre;
-	copyButton.setAttribute(
-		'class',
-		'mw-ui-button pretoclip-copy-button'
-	);
-	copyButton.setAttribute(
-		'title',
-		mw.message( 'pretoclip-button-tooltip' ).text()
-	);
-	copyButton.innerText = mw.message( 'pretoclip-button-label' ).text();
-	pre.parentNode.insertBefore( copyButton, pre );
-
-	var copyButtonRect = copyButton.getBoundingClientRect(),
-		offsetLeft = ( preUpperRightCornerX - copyButtonRect.width );
-	var style =
-	'display:none;' +
-	'z-index:1000;' +
-	'position:absolute;' +
-	'left:' + offsetLeft + 'px;';
-	copyButton.setAttribute( 'style', style );
-
-	pre.preToClipButton = copyButton;
-}
-
-$( document ).on( 'mouseover', 'pre', function () {
-	if ( !this.preToClipButton ) {
-		createPreToClipButton( this );
+( function ( mw, $ ) {
+	'use strict';
+	if ( !navigator.clipboard ) {
+		// eslint-disable-next-line no-console
+		console.error( 'Extension:PreToClip: `navigator.clipboard` not available. HINT: Only works from HTTPS or `localhost` context.' );
+		//return;
 	}
-	this.preToClipButton.style.display = 'block';
-} );
+	function createPreToClipButton( pre ) {
+		var copyButton = document.createElement( 'button' );
 
-$( document ).on( 'mouseover', '.pretoclip-copy-button', function () {
-	this.isMouseOver = true;
-} );
+		copyButton.assignedPre = pre;
+		copyButton.setAttribute(
+			'class',
+			'mw-ui-button pretoclip-copy-button'
+		);
+		copyButton.setAttribute(
+			'title',
+			mw.message( 'pretoclip-button-tooltip' ).text()
+		);
+		copyButton.innerText = mw.message( 'pretoclip-button-label' ).text();
+		pre.parentNode.insertBefore( copyButton, pre );
 
-$( document ).on( 'mouseout', '.pretoclip-copy-button', function () {
-	this.isMouseOver = false;
-	this.style.display = 'none';
-} );
+		var style =
+		'display:none;' +
+		'z-index:1000;' +
+		'position:absolute;' +
+		'right:0';
+		copyButton.setAttribute( 'style', style );
 
-$( document ).on( 'mouseout', 'pre', function () {
-	var me = this;
-	setTimeout( function () {
-		if ( me.preToClipButton && !me.preToClipButton.isMouseOver ) {
-			me.preToClipButton.style.display = 'none';
+		pre.preToClipButton = copyButton;
+	}
+
+	$( document ).on( 'mouseover', 'pre', function () {
+		if ( !this.preToClipButton ) {
+			createPreToClipButton( this );
 		}
-	},
-	50
-	);
-} );
+		this.preToClipButton.style.display = 'block';
+	} );
 
-$( document ).on( 'click', '.pretoclip-copy-button', function () {
-	// eslint-disable-next-line compat/compat
-	navigator.clipboard.writeText( this.assignedPre.innerText );
-	mw.notify( mw.message( 'pretoclip-button-notification-text' ).text() );
-} );
+	$( document ).on( 'mouseover', '.pretoclip-copy-button', function () {
+		this.isMouseOver = true;
+	} );
+
+	$( document ).on( 'mouseout', '.pretoclip-copy-button', function () {
+		this.isMouseOver = false;
+		this.style.display = 'none';
+	} );
+
+	$( document ).on( 'mouseout', 'pre', function () {
+		var me = this;
+		setTimeout( function () {
+			if ( me.preToClipButton && !me.preToClipButton.isMouseOver ) {
+				me.preToClipButton.style.display = 'none';
+			}
+		},
+		50
+		);
+	} );
+
+	$( document ).on( 'click', '.pretoclip-copy-button', function () {
+		// eslint-disable-next-line compat/compat
+		navigator.clipboard.writeText( this.assignedPre.innerText );
+		mw.notify( mw.message( 'pretoclip-button-notification-text' ).text() );
+	} );
+
+	// eslint-disable-next-line no-jquery/no-global-selector
+	if ( $( 'pre' ).length > 0 ) {
+		mw.loader.load( [ 'mediawiki.ui.button' ] );
+	}
+}( mediaWiki, jQuery ) );
