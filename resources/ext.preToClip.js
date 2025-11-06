35bc6ce3cb1533fb6292144cd03e57f642e11705
hslater <hamish.slater@hallowelt.com>
2025-11-07 00:49:43 +0700

    Remove non-breaking spaces from copied text
    ERM44290
    
    [5.1.x]
    
    Change-Id: Ie3908b16957401374dee1d2c3aae10f3670d0981

--- resources/ext.preToClip.js
+++ resources/ext.preToClip.js
@@ -1,41 +1,30 @@
 ( function ( mw ) {
 	'use strict';
 	if ( !navigator.clipboard ) {
-		// eslint-disable-next-line no-console
-		console.error( 'Extension:PreToClip: `navigator.clipboard` not available. HINT: Only works from HTTPS or `localhost` context.' );
+		console.error( 'Extension:PreToClip: `navigator.clipboard` not available. HINT: Only works from HTTPS or `localhost` context.' ); // eslint-disable-line no-console
 		return;
 	}
 
-	const preElements = document.querySelectorAll( '#content pre' );
-	for ( let i = 0; i < preElements.length; i++ ) {
-		createPreToClipButton( preElements[ i ] );
-	}
-
-	function createPreToClipButton( pre ) {
+	document.querySelectorAll( '#content pre' ).forEach( ( pre ) => { // eslint-disable-line mediawiki/no-nodelist-unsupported-methods
 		if ( !pre.getAttribute( 'tabindex' ) ) {
 			pre.setAttribute( 'tabindex', '0' );
 		}
 
-		const clipboardText = pre.innerText;
 		const tooltip = mw.message( 'pretoclip-button-tooltip' ).text();
 
 		const copyButton = new OO.ui.ButtonWidget( {
 			icon: 'copy',
 			classes: [ 'pretoclip-copy-button' ],
 			tabIndexed: true,
-			title: tooltip,
-			data: {
-				clipboardText: clipboardText
-			}
+			title: tooltip
 		} );
 
-		copyButton.on( 'click', function () {
-			const data = this.getData();
-			// eslint-disable-next-line compat/compat
-			navigator.clipboard.writeText( data.clipboardText );
+		copyButton.on( 'click', () => {
+			const text = pre.innerText.replace( /\u00A0/g, ' ' );
+			navigator.clipboard.writeText( text ); // eslint-disable-line compat/compat
 			mw.notify( mw.message( 'pretoclip-button-notification-text' ).text() );
-		}, [], copyButton );
+		} );
 
 		pre.prepend( copyButton.$element[ 0 ] );
-	}
+	} );
 }( mediaWiki ) );
